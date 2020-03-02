550bb3a2c628c95990ef2fe59c533130e0ca13eb
ChrisW3 <cwool33@yahoo.com>
2020-03-01 22:53:54 -0500

    Create Instate.java

---
+++ Instate.java
@@ -0,0 +1,23 @@
+
+public class Instate extends Student{
+	private int funds;
+	
+	public Instate(String fname, String lname, int credit, int f) {
+		super(fname, lname, credit);
+		funds = f;
+	}
+
+	@Override
+	public int tuitionDue() {
+		if(credit > 15) {
+			return 433 * 15 + 1441 - funds;
+		}
+		else if(credit >= 12) {
+			return 433 * credit + 1441 - funds;
+		}
+		else {
+			return 433 * credit + 846;
+		}
+			
+	}
+}
